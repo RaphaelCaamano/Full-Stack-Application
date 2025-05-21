ef5595be021c0bf4377634622216ead70d44105f
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-21 07:37:10 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Optional CSS import
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
