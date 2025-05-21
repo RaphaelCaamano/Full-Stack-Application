f54029f200d0d3c5d1bf17845a0586b0dabd0c6a
Raphael Caamano <155185862+RaphaelCaamano@users.noreply.github.com>
2025-05-21 06:44:55 -0400

    Update Item.js

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
 
+// Define the Item schema
+const ItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the Item model
+const Item = mongoose.model('Item', ItemSchema);
+
+module.exports = Item;
